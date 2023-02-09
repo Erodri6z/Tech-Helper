d7cf47f51555885a586c85f9d2f5caf34320094c
Erodri6z <erikrodriguez2417@gmail.com>
2023-02-08 21:16:42 -0600

    add a footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <ul>
+        <li>About</li>
+        <li>IOS</li>
+        <li>ANDROID</li>
+      </ul>
+    </>
+  )
+}
+
+export default Footer
