0a461033a20ed5e0331ee6b22d00dc4256a1a052
Erodri6z <erikrodriguez2417@gmail.com>
2023-02-09 00:22:21 -0600

    add the pge and link to the android page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router';
 import Footer from './components/Footer/Footer';
 import About from './pages/About/About';
 import IOS from './pages/IOS/IOS';
+import Android from './pages/Android/Android';
 
 function App() {
   return (
@@ -19,6 +20,12 @@ function App() {
     element={
       <IOS />
     }
+    />
+    <Route
+    path='/android'
+    element={
+      <Android />
+    } 
     />
   </Routes>
   <Footer />
---
+++ src/pages/Android/Android.jsx
@@ -0,0 +1,11 @@
+const Android = () => {
+  return (
+    <>
+      <h1>This is where the Android stuff is gonna be</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam fugiat suscipit natus esse earum? Quo, qui, ducimus recusandae vel voluptas quam sequi iste cupiditate, corrupti rerum ad maxime aspernatur molestias.
+      </p>
+    </>
+  )
+}
+
+export default Android
