78fc980e43c37427727d7b57e25acf455006d217
Erodri6z <erikrodriguez2417@gmail.com>
2023-02-22 22:49:43 -0600

    add css to options

--- src/pages/Android/Android.jsx
+++ src/pages/Android/Android.jsx
@@ -1,19 +1,23 @@
+import "./Android.css"
+
 const Android = () => {
   return (
     <>
       <h1>This is where the Android stuff is gonna be</h1>
-      <div className="h-options">
-        <a href="#">Customization</a>
-      </div>
-      <div className="h-options">
-        <a href="#">Troubleshoot</a>
-      </div>
-      <div className="h-options">
-        <a href="#">Management</a>
-      </div>
-      <div className="h-options">
-        <a href="#">Forum</a>
-      </div>
+      <section className="selection">
+        <div className="a-options">
+          <a href="#">Customization</a>
+        </div>
+        <div className="a-options">
+          <a href="#">Troubleshoot</a>
+        </div>
+        <div className="a-options">
+          <a href="#">Management</a>
+        </div>
+        <div className="a-options">
+          <a href="#">Forum</a>
+        </div>
+      </section>
     </>
   )
 }
--- src/pages/IOS/IOS.jsx
+++ src/pages/IOS/IOS.jsx
@@ -1,19 +1,23 @@
+import "./IOS.css"
+
 const IOS = () => {
   return (
     <>
     <h1>Hello this is where the ios Stuff will be</h1>
-    <div className="h-options">
-      <a href="#">Customization</a>
-    </div>
-    <div className="h-options">
-      <a href="#">Troubleshoot</a>
-    </div>
-    <div className="h-options">
-      <a href="#">Management</a>
-    </div>
-    <div className="h-options">
-      <a href="#">Forum</a>
-    </div>
+    <section className="options">
+      <div className="i-options">
+        <a href="#">Customization</a>
+      </div>
+      <div className="i-options">
+        <a href="#">Troubleshoot</a>
+      </div>
+      <div className="i-options">
+        <a href="#">Management</a>
+      </div>
+      <div className="i-options">
+        <a href="#">Forum</a>
+      </div>
+    </section>
     </>
   )
 }
