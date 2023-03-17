5e8c27b39d63b626cb3235b3e3acee8042b25160
Erodri6z <erikrodriguez2417@gmail.com>
2023-03-17 00:02:28 -0500

    stub up a post form

---
+++ src/components/Postform/PostForm.jsx
@@ -0,0 +1,43 @@
+const PostForm = (props) => {
+  const [formData, setFormData] = useState({
+    question: '',
+    elaboration:'',
+    os: '',
+  })
+  const handleChange = (e) => {
+    setFormData({...formData,
+      [e.target.name] : e.target.value})
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try{
+      props.handleAddNote(formData)
+      Navigate('/journal')
+    }catch (err) {
+      console.log(err)
+    }
+  }
+
+  return(
+    <div className="post-entry">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Whats on your mind?</p>
+        </label>
+        <input type="text" name="question" id="question-box" />
+        <label>
+          <p>Elaborate</p>
+        </label>
+        <input type="text" name="elaboration" is="elaboration-box" />
+        <select name="os" id="os-select">
+          <option value="IOS">IOS</option>
+          <option value="ANDROID">Android</option>
+        </select>
+      </form>
+    </div>
+  )
+
+}
+
+export default PostForm
