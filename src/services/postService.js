7809d0b0df3ef4fc5f604a8321d08d60b9f601c9
Erodri6z <erikrodriguez2417@gmail.com>
2023-03-25 00:30:38 -0500

    add a delete function on post service app

--- src/services/postService.js
+++ src/services/postService.js
@@ -21,6 +21,14 @@ async function create(post) {
   return await res.json()
 }
 
+async function deletePost(postId) {
+  const res = await fetch(`${BASE_URL}/${postId}`, {
+    method: 'DELETE',
+    headers: { 'Authorization' : `Bearer ${tokenService.getToken()}`},
+  })
+  return await res.json()
+}
+
 async function getPost(postId) {
   const res = await fetch(`${BASE_URL}/${postId}`,{
     headers: { Authorization: `Bearer ${tokenService.getToken()}`},
@@ -31,5 +39,6 @@ async function getPost(postId) {
 export {
   getAll,
   create,
-  getPost
+  getPost,
+  deletePost
 }
