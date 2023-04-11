8b8f56efbc0b5e4dd94fb56e9c8b51f6b8a34c76
Erodri6z <erikrodriguez2417@gmail.com>
2023-04-11 12:36:14 -0500

    limit the amount of the post you see when viewing a forum

--- src/pages/Forum/AForum.jsx
+++ src/pages/Forum/AForum.jsx
@@ -5,6 +5,7 @@ import './Forum.css'
 const AForum = (props) => {
   const posts = props.posts
   const androidPosts = posts.filter(p => p.os === 'ANDROID')
+  const maxLength = 250
   return (
     <>
     <h1>Android Forum</h1>
@@ -21,7 +22,11 @@ const AForum = (props) => {
           <h3>{p.poster.name} Asks : "{p.question}"</h3>
         </div>
         <div className="question">
+          {p.elaboration.length > maxLength ?
+          <p>{p.elaboration.substring(0, maxLength)}...</p>
+          :
           <p>{p.elaboration}</p>
+          }
         </div>
       </div>
       <hr />
