9e35abc1da5a51e2bb4121729541dfe879a8a6a8
Erodri6z <erikrodriguez2417@gmail.com>
2023-04-11 12:59:16 -0500

    set the same changes towards ios forum

--- src/pages/Forum/IForum.jsx
+++ src/pages/Forum/IForum.jsx
@@ -6,26 +6,38 @@ import './Forum.css'
 const IForum = (props) => {
   const posts = props.posts
   const iosPosts = posts.filter(p => p.os === 'IOS')
+  const maxLengthElaboration = 250
+  const maxLengthQuestion = 75
   return (
     <>
     <h1>IOS Forum</h1>
     <div className="forum">
-    
     { props.user ?
     <PostForm handleAddPost={props.handleAddPost}/>
     :
     <h4>Want to post?</h4>
   }
-      {iosPosts.map(p => 
-      <Link to='/post' key={p._id} state={{ p }}>
-      <div key={p._id} className='i-post'>
-        <h3>{p.poster.name} Asks:</h3>
-        <h4>{p.question}</h4>
-        <p>{p.elaboration}</p>
+    {iosPosts.map(p => 
+    <Link to='/post' key={p._id} className='post-link' state={{ p }}>
+      <div key={p._id} className='a-post'>
+        <div className="post-header">
+          {p.question.length > maxLengthQuestion ?
+          <h3>{p.poster.name} Asks : "{p.question.substring(0, maxLengthQuestion)}..."</h3>
+          :
+          <h3>{p.poster.name} Asks : "{p.question}"</h3>
+        }
+        </div>
+        <div className="question">
+          {p.elaboration.length > maxLengthElaboration ?
+          <p>{p.elaboration.substring(0, maxLengthElaboration)}...</p>
+          :
+          <p>{p.elaboration}</p>
+          }
+        </div>
       </div>
       <hr />
     </Link>
-      )}
+    )}
     </div>
     </>
   )
