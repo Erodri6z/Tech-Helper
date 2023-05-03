329a45d6761952e7d9b47b7eacea00941856a4aa
Erodri6z <erikrodriguez2417@gmail.com>
2023-05-03 13:35:36 -0500

    add cn actual about me to the about me page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <>
     <h1>Hi There</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, voluptate suscipit? Quod asperiores praesentium facere hic omnis quis eius ratione delectus quo est quos totam, nulla veniam ullam laudantium aliquam!</p>
+    <p>Introducing a new app that is designed to help people who are not tech savvy with their phones. This app is packed with basic troubleshooting tips and tricks that are easy to follow, even for those who are not very familiar with technology. The app provides step-by-step instructions for fixing common phone problems such as slow performance, battery drain, and software glitches. In addition to the troubleshooting tips, the app also features a Q&A forum where users can ask questions and get answers from a community of knowledgeable experts. The forum is moderated to ensure that all answers are accurate and helpful. With this app, anyone can become a tech-savvy phone user and solve their own problems without needing to rely on expensive tech support.</p>
     </>
   )
 }
