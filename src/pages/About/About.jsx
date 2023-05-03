750906083891cb0461818ca33b597533a3ef5594
Erodri6z <erikrodriguez2417@gmail.com>
2023-05-03 13:46:23 -0500

    add some css to about me page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,10 @@
+import './About.css'
+
 const About = () => {
   return (
     <>
-    <h1>Hi There</h1>
-    <p>Introducing a new app that is designed to help people who are not tech savvy with their phones. This app is packed with basic troubleshooting tips and tricks that are easy to follow, even for those who are not very familiar with technology. The app provides step-by-step instructions for fixing common phone problems such as slow performance, battery drain, and software glitches. In addition to the troubleshooting tips, the app also features a Q&A forum where users can ask questions and get answers from a community of knowledgeable experts. The forum is moderated to ensure that all answers are accurate and helpful. With this app, anyone can become a tech-savvy phone user and solve their own problems without needing to rely on expensive tech support.</p>
+    <h1 id='about-me-title'>Hi There</h1>
+    <p id='about-me'>Introducing a new app that is designed to help people who are not tech savvy with their phones. This app is packed with basic troubleshooting tips and tricks that are easy to follow, even for those who are not very familiar with technology. The app provides step-by-step instructions for fixing common phone problems such as slow performance, battery drain, and software glitches. In addition to the troubleshooting tips, the app also features a Q&A forum where users can ask questions and get answers from a community of knowledgeable experts. The forum is moderated to ensure that all answers are accurate and helpful. With this app, anyone can become a tech-savvy phone user and solve their own problems without needing to rely on expensive tech support.</p>
     </>
   )
 }
