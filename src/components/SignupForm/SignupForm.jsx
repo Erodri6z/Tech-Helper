e6b3d97ea7f4af6c8d82720a20932c3952191a5b
Erodri6z <erikrodriguez2417@gmail.com>
2023-05-16 16:47:36 -0500

    add the same fonts to forms

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -94,8 +94,9 @@ const SignupForm = props => {
           onChange={handleChange}
         />
       </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor="photo-upload" className={styles.label}>
+      {/* gonna save this because not sure if I want this feature yet */}
+      {/* <div className={styles.inputContainer}> */}
+        {/* <label htmlFor="photo-upload" className={styles.label}>
           Upload Photo
         </label>
         <input
@@ -104,7 +105,7 @@ const SignupForm = props => {
           name="photo"
           onChange={handleChangePhoto}
         />
-      </div>
+      </div> */}
       <div className={styles.inputContainer}>
         <button disabled={isFormInvalid()} className={styles.button}>
           Sign Up
