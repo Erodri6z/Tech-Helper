31d39e759b048940c96e734e104e673148cbfcb4
Erodri6z <erikrodriguez2417@gmail.com>
2023-05-28 20:49:26 -0500

    add a functioning toggle button for the nav

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom'
 import React from 'react'
 
 const Head = ({user, handleLogout}) => {
+
+  const toggleNav = () => {
+    const navBar = document.getElementById('nav')
+    navBar.classList.toggle('active')
+  }
+
   return (
   <>
     <div className="heading">
       <h1 className="title">Pocket Geeks</h1>
-      <button className='nav-select'>
+      <button className='nav-select' onClick={toggleNav}>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
         </svg>
