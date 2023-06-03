9738279abecd1e72a247445f858f652c5656806c
Erodri6z <erikrodriguez2417@gmail.com>
2023-06-03 18:38:59 -0500

    add css to the nav in smaller screens

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -18,30 +18,30 @@ const Head = ({user, handleLogout}) => {
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
         </svg>
       </button>
-      <div id='nav'>
-        <ul>
-          <li>Android</li>
-          <li>Apple</li>
-          <li>About</li>
-          {user?
-          <li>Log Out</li>
-          :
-          <>
-          <li>Login</li>
-          <li>Sign Up</li>
-          </>
-          }
-        </ul>
-      </div>
       {user?
       <li ><Link to="" onClick={handleLogout} className='logout'>Logout</Link></li>
       :
       <>
-      <li ><Link className='signon' to="/signup" >Sign Up</Link></li>
-      <li ><Link className='signon' to="/login" >Log In</Link></li>
+      <li ><Link className='nav-item fullscreen-signon' to="/signup" >Sign Up</Link></li>
+      <li ><Link className='nav-item fullscreen-signon' to="/login" >Log In</Link></li>
       </>
       }
     </div>
+      <div id='nav'>
+        <ul>
+          <li><Link className='nav-item'>Android</Link></li>
+          <li><Link className='nav-item'>Apple</Link></li>
+          <li><Link className='nav-item'>About</Link></li>
+          {user?
+          <li><Link to="" onClick={handleLogout} className='logout'>Log Out</Link></li>
+          :
+          <>
+          <li><Link className='nav-item' to="/signup" >Sign Up</Link></li>
+          <li><Link className='nav-item' to="/login" >Log In</Link></li>
+          </>
+          }
+        </ul>
+      </div>
   </>
   )
 }
